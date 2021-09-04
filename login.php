f33f9d6b62a734a5e6d52299f483fe90c2abd4a3
N-Hanil <71916269+N-Hanil@users.noreply.github.com>
2021-09-04 10:17:32 +0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+$connection = mysqli_connect('localhost','root','','facebookclone');
+ 
+$phoneoremail = $_POST['phoneoremail'];
+$password = $_POST['password'];
+
+$check_data = mysqli_query($connection, "SELECT * FROM userdata WHERE phoneoremail = '$phoneoremail'");
+$check = mysqli_num_rows($check_data);
+
+if($check > 0){
+echo "Email already exist";
+}
+else{
+$input = mysqli_query($connection, "INSERT INTO userdata(id,firstname,lastname,phoneoremail,password) VALUES ('$id','$firstname','$lastname','$phoneoremail','$password')");
+if($input){
+echo "Signup Successful";
+}
+else{
+echo "Error Occured";
+}
+}
+?>
